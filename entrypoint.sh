1cab3610c083d461b66b63355b1e5c5840cc7d20
Vadim Sidorov <v.sidorov@anchorfree.com>
2017-10-23 03:12:51 -0700

    SRV-3487 possibility to override MATCH_REGEXP and to execute a hook at a startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,34 @@ if [ -z ${LOCAL_PATH} ]; then
 	exit 1
 fi
 
-MATCH_REGEXP=$(cat /etc/af/array_name).*${MATCH_REGEXP}
+if [ -z ${AWS_ACCESS_KEY_ID} ]; then
+	echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are mandatory"
+    exit 1
+fi
 
-while true; do
+if [ -z ${AWS_SECRET_ACCESS_KEY} ]; then
+	echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are mandatory"
+    exit 1
+fi
+
+AWS_REGION=${AWS_REGION:-'us-east-1'}
+
+if [ -z "${MATCH_REGEXP_OVERRIDE}" ]; then
+    MATCH_REGEXP=$(cat /etc/af/array_name).*${MATCH_REGEXP}
+else
+    MATCH_REGEXP="${MATCH_REGEXP_OVERRIDE}"
+fi
+
+export MATCH_REGEXP AWS_REGION
+
+ALIVE=1
+trap ALIVE=0 SIGTERM
+
+if [ ! -z ${EXEC_ON_START} ]; then
+    ${EXEC_ON_START}
+fi
+
+while [ "$ALIVE" -eq "1" ]; do
     /s3sync sync ${S3_PATH} ${LOCAL_PATH}
     sleep ${TIMEOUT} &
     wait
