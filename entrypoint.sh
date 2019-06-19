ac1b28e6c9bfe84d7e25ab49fcfcf8654fafa848
Dmytro Lukianchikov <d.lukyanchikov@anchorfree.com>
2019-06-19 12:23:28 -0700

    add AWS_CLOUD_ENVIRONMENT env variable, when true AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY is not mandatory

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,18 @@ if [ -z ${CREATE_K8S_SECRET} ] || [ ${CREATE_K8S_SECRET} != "true" ]; then
         fi
 fi
 
+
+
+if [ -z ${AWS_ACCESS_KEY_ID} ] && [ -z ${AWS_CLOUD_ENVIRONMENT} ]; then
+        echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are mandatory"
+    exit 1
+fi
+
+if [ -z ${AWS_SECRET_ACCESS_KEY} ] && [ -z ${AWS_CLOUD_ENVIRONMENT} ]; then
+        echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are mandatory"
+    exit 1
+fi
+
 AWS_REGION=${AWS_REGION:-'us-east-1'}
 
 if [ -z "${MATCH_REGEXP_OVERRIDE}" ]; then
