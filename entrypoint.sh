8c21735a6af0131b05b4422607b2b23b4be98b11
Dmytro Lukianchikov <d.lukyanchikov@anchorfree.com>
2019-06-19 00:41:50 -0700

    remove checks for aws env varaible for auth, it allow to use this app on aws nstances without providing any additional creds

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,16 +14,6 @@ if [ -z ${CREATE_K8S_SECRET} ] || [ ${CREATE_K8S_SECRET} != "true" ]; then
         fi
 fi
 
-if [ -z ${AWS_ACCESS_KEY_ID} ]; then
-        echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are mandatory"
-    exit 1
-fi
-
-if [ -z ${AWS_SECRET_ACCESS_KEY} ]; then
-        echo "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are mandatory"
-    exit 1
-fi
-
 AWS_REGION=${AWS_REGION:-'us-east-1'}
 
 if [ -z "${MATCH_REGEXP_OVERRIDE}" ]; then
